e5eb4212b6ef89b4e2db4d80d581986d387f2512
Vnixian <112727153+Vnixian@users.noreply.github.com>
2022-09-14 19:41:10 +0000

    Update

---
+++ k29.py
@@ -0,0 +1,47 @@
+from stanfordkarel import *
+
+
+def main():
+    """ Karel code goes here! """
+    move()
+    move()
+    move()
+    move()
+    turn_left()
+    move()
+    move()
+    pick_beeper()
+    move()
+    put_beeper()
+    pick_beeper()
+    move()
+    put_beeper()
+    pick_beeper()
+    move()
+    put_beeper()
+    pick_beeper()
+    move()
+    put_beeper()
+    turn_left()
+    move()
+    turn_left()
+    pick_beeper()
+    move()
+    put_beeper()
+    pick_beeper()
+    move()
+    put_beeper()
+    pick_beeper()
+    move()
+    put_beeper()
+    pick_beeper()
+    move()
+    put_beeper()
+    move()
+    move()
+    turn_left()
+    pass
+
+
+if __name__ == "__main__":
+    run_karel_program()
