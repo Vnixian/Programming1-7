90945c549e43f9e173e5b2b3465125d770aafb3a
Vnixian <112727153+Vnixian@users.noreply.github.com>
2022-09-23 19:26:32 +0000

    Mwua hahaha

---
+++ k34.py
@@ -0,0 +1,130 @@
+from stanfordkarel import *
+
+
+class ktools:
+  def m(self):
+    """Shorthand for Move"""
+    move()
+
+  def tl(self):
+    """Turn Left"""
+    turn_left()
+
+  def tr(self):
+    """Turn Right"""
+    self.tl()
+    self.tl()
+    self.tl()
+
+  def ta(self):
+    """Turn Around"""
+    self.tl()
+    self.tl()
+
+  def pick(self):
+    """Pick Beeper"""
+    pick_beeper()
+
+  def put(self):
+    """Put Beeper"""
+    put_beeper()
+
+  def put2(self):
+    """Put 2 beepers in a line"""
+    self.put()
+    self.m()
+    self.put()
+
+  def put5(self):
+    """Put 5 beepers in a line"""
+    self.put2()
+    self.m()
+    self.put2()
+    self.m()
+    self.put()
+
+  def h(self):
+    """Print H using beepers"""
+    self.tl()
+    self.put5()
+    self.tr()
+    self.m()
+    self.m()
+    self.m()
+    self.tr()
+    self.put5()
+    self.ta()
+    self.m()
+    self.m()
+    self.tl()
+    self.m()
+    self.put2()
+    self.tl()
+    self.m()
+    self.m()
+    self.tl()
+    self.m()
+    self.m()
+    self.m()
+    self.m()
+    
+
+def main():
+    """ Karel code goes here! """
+    kt = ktools()
+    kt.m()
+    kt.m()
+    kt.m()
+    kt.m()
+    kt.tl()
+    kt.m()
+    kt.m()
+    pick_beeper()
+    kt.m()
+    pick_beeper()
+    kt.m()
+    pick_beeper()
+    kt.m()
+    pick_beeper()
+    kt.m()
+    pick_beeper()
+    kt.tr()
+    kt.m()
+    kt.tl()
+    kt.tl()
+    kt.tl()
+    kt.m()
+    pick_beeper()
+    kt.m()
+    pick_beeper()
+    kt.m()
+    pick_beeper()
+    kt.tl()
+    kt.m()
+    kt.tl()
+    kt.m()
+    pick_beeper()
+    kt.tl()
+    kt.m()
+    kt.m()
+    kt.m()
+    pick_beeper()
+    kt.tl()
+    kt.m()
+    pick_beeper()
+    kt.tl()
+    kt.tl()
+    kt.m()
+    kt.m()
+    pick_beeper()
+    kt.tl()
+    kt.m()
+    kt.tl()
+    kt.m()
+    pick_beeper()
+    kt.tl()
+    pass
+
+
+if __name__ == "__main__":
+    run_karel_program()
